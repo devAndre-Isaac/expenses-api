df961fb29984e562c286f7daca3354509cb5ecb5
André Isaac <andrepinto.6767@aluno.una.br>
2023-02-12 21:27:12 -0300

    feat: add User entity

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,52 @@
+import { Expenses } from 'src/modules/expenses/entities/expense.entity';
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('users')
+export class User {
+  @OneToMany((type) => Expenses, (despesa) => despesa.user)
+  @JoinColumn()
+  expenses: Expenses[];
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  cpf: string;
+
+  @Column()
+  telephoneNumber: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) {
+      return;
+    }
+
+    this.id = uuidv4();
+  }
+}
