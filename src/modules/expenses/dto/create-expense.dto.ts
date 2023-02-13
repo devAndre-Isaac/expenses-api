bccc457c39b5f1b2dd9439f3177848c336fd7975
André Isaac <andrepinto.6767@aluno.una.br>
2023-02-12 21:23:56 -0300

    feat: add CreateExpenseDto

---
+++ src/modules/expenses/dto/create-expense.dto.ts
@@ -0,0 +1,48 @@
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsFuture } from 'src/utils/decorators';
+
+export class CreateExpenseDto {
+  @ApiProperty({
+    description: 'description',
+    example: 'description of expense',
+  })
+  @IsNotEmpty()
+  @MaxLength(191, { message: 'Description must be less than 191 characters' })
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    description: 'date',
+    example: 'date of expense',
+  })
+  @IsDate()
+  @IsFuture()
+  @IsNotEmpty()
+  date: Date;
+
+  @ApiProperty({
+    description: 'value',
+    example: 'value of expense',
+  })
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  value: number;
+
+  @ApiProperty({
+    description: 'userId',
+    example: 'identify of user associate with expense create',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  userId: string;
+}
